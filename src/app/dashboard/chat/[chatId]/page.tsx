b7d493350e93e9b1d347e407df2bcecb161c965d
Alex Sherman <asherman.ca@gmail.com>
2023-08-13 20:19:47 -0700

    component - message

--- src/app/dashboard/chat/[chatId]/page.tsx
+++ src/app/dashboard/chat/[chatId]/page.tsx
@@ -1,6 +1,8 @@
+import Messages from '@/components/Messages'
 import { fetchRedis } from '@/helpers/redis'
 import { getAuthSession } from '@/lib/auth'
 import { messageArrayValidator } from '@/lib/validations/message'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 async function getChatMessages(chatId: string) {
@@ -41,10 +43,52 @@ const page = async ({ params }: pageProps) => {
 	if (user.id !== userId1 && user.id !== userId2) notFound()
 
 	const chatPartnerId = user.id === userId1 ? userId2 : userId1
-	const chatPartner = (await fetchRedis('get', `user:${chatPartnerId}`)) as User
-	const messages = await getChatMessages(chatId)
+	const chatPartnerRaw = (await fetchRedis(
+		'get',
+		`user:${chatPartnerId}`
+	)) as string
+	const chatPartner = JSON.parse(chatPartnerRaw) as User
 
-	return <div>{chatId}</div>
+	const messages = await getChatMessages(chatId)
+	console.log(chatPartner)
+
+	return (
+		<div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+			<div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+				<div className='relative flex items-center space-x-4'>
+					<div className='relative'>
+						<div className='relative w-8 sm:w-12 h-8 sm:h-12'>
+							<Image
+								fill
+								referrerPolicy='no-referrer'
+								src={chatPartner.image}
+								alt={`${chatPartner.name} profile picture`}
+								className='rounded-full'
+							/>
+						</div>
+					</div>
+
+					<div className='flex flex-col leading-tight'>
+						<div className='text-xl flex items-center'>
+							<span className='text-gray-700 mr-3 font-semibold'>
+								{chatPartner.name}
+							</span>
+						</div>
+
+						<span className='text-sm text-gray-600'>{chatPartner.email}</span>
+					</div>
+				</div>
+			</div>
+
+			<Messages
+				initialMessages={messages}
+				sessionId={session.user.id}
+				chatPartner={chatPartner}
+				sessionImg={session.user.image}
+				chatId={chatId}
+			/>
+		</div>
+	)
 }
 
 export default page
