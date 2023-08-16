8ca0606d009d5025cbb2facd1fc862aaaa06b4e5
Alex Sherman <asherman.ca@gmail.com>
2023-08-16 12:43:04 -0700

    feature - friend request pusher events

--- src/components/FriendRequestSidebarOptions.tsx
+++ src/components/FriendRequestSidebarOptions.tsx
@@ -1,7 +1,9 @@
 'use client'
 import Link from 'next/link'
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { User } from 'lucide-react'
+import { pusherClient } from '@/lib/pusher'
+import { toPusherKey } from '@/lib/utils'
 
 interface FriendRequestSidebarOptionsProps {
 	sessionId: string
@@ -15,6 +17,34 @@ const FriendRequestSidebarOptions: FC<FriendRequestSidebarOptionsProps> = ({
 	const [unseenRequestCount, setUnseenRequestCount] = useState<number>(
 		initialUnseenRequestCount
 	)
+
+	useEffect(() => {
+		pusherClient.subscribe(
+			toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+		)
+		pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
+
+		const friendRequestHandler = () => {
+			setUnseenRequestCount((prev) => prev + 1)
+		}
+
+		const addedFriendHandler = () => {
+			setUnseenRequestCount((prev) => prev - 1)
+		}
+
+		pusherClient.bind('incoming_friend_requests', friendRequestHandler)
+		pusherClient.bind('new_friend', addedFriendHandler)
+
+		return () => {
+			pusherClient.unsubscribe(
+				toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+			)
+			pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`))
+
+			pusherClient.unbind('new_friend', addedFriendHandler)
+			pusherClient.unbind('incoming_friend_requests', friendRequestHandler)
+		}
+	}, [sessionId])
 
 	// console.log('unseenRequestCount', unseenRequestCount)
 	// console.log('initialUnseenRequestCount', initialUnseenRequestCount)
--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { pusherClient } from '@/lib/pusher'
+import { toPusherKey } from '@/lib/utils'
 import axios from 'axios'
 import { Check, UserPlus, X } from 'lucide-react'
 import { useRouter } from 'next/navigation'
@@ -17,6 +19,29 @@ const FriendRequests: FC<FriendRequestsProps> = ({
 	const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
 		incomingFriendRequests
 	)
+
+	useEffect(() => {
+		pusherClient.subscribe(
+			toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+		)
+
+		const friendRequestHandler = ({
+			senderId,
+			senderEmail,
+		}: IncomingFriendRequest) => {
+			console.log('function got called')
+			setFriendRequests((prev) => [...prev, { senderId, senderEmail }])
+		}
+
+		pusherClient.bind('incoming_friend_requests', friendRequestHandler)
+
+		return () => {
+			pusherClient.unsubscribe(
+				toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+			)
+			pusherClient.unbind('incoming_friend_requests', friendRequestHandler)
+		}
+	}, [])
 
 	const acceptFriend = async (senderId: string) => {
 		await axios.post('/api/friends/accept', { id: senderId })
